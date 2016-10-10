90452fec47addeaf6bfc1d4e5540b40526b5de56
Thomas Goirand <zigo@debian.org>
2016-10-10 13:56:37 +0200

    Correctly kills mongodb processes (Closes: #835177)
    Change-Id: Id3f717fb708c99ff808601a5c73f9911afa069a0

--- debian/setup-test-env-mongodb.sh
+++ debian/setup-test-env-mongodb.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -e
 
-source debian/functions.sh
-
-if [ "$1" = "--coverage" ]; then
-	COVERAGE_ARG="$1"
-	shift
-fi
-
-export PATH=${PATH:+$PATH:}/sbin:/usr/sbin
-check_for_cmd mongod
+wait_for_line () {
+	while read line ; do
+		echo "$line" | grep -q "$1" && break
+	done < "$2"
+	# Read the fifo for ever otherwise process would block
+	cat "$2" >/dev/null &
+	WAIT_FOR_LINE_PID=$!
+}
 
 # Start MongoDB process for tests
 MONGO_DATA=`mktemp -d /tmp/AODH-MONGODB-XXXXX`
 MONGO_PORT=29000
-trap "clean_exit ${MONGO_DATA}" EXIT
 mkfifo ${MONGO_DATA}/out
 mongod --maxConns 32 --nojournal --noprealloc --smallfiles --quiet --noauth --port ${MONGO_PORT} --dbpath "${MONGO_DATA}" --bind_ip localhost --config /dev/null &>${MONGO_DATA}/out &
+MONGO_PID=$!
 # Wait for Mongo to start listening to connections
 wait_for_line "waiting for connections on port ${MONGO_PORT}" ${MONGO_DATA}/out
 # Read the fifo for ever otherwise mongod would block
 cat ${MONGO_DATA}/out > /dev/null &
+MONGO_DATA_PID=$!
 export AODH_TEST_STORAGE_URL="mongodb://localhost:${MONGO_PORT}/AODH"
 
 # Yield execution to venv command
 OS_TEST_PATH=./aodh/tests/unit $*
+
+# Kill all processes
+kill ${WAIT_FOR_LINE_PID}
+kill ${MONGO_PID}
+kill ${MONGO_DATA_PID}
